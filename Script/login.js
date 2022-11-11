b6b0f7498be85f07b9ded54c74d6cb878df208ff
GauriSandhan <89839207+GauriSandhan@users.noreply.github.com>
2022-11-11 18:51:48 +0530

    opi

--- Script/login.js
+++ Script/login.js
@@ -1,1 +1,26 @@
-let op=document.getElementById('asp')
+var store_data=JSON.parse(localStorage.getItem("userData"))
+
+var form=document.querySelector("form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    var data={
+        email:form.email.value,
+        password:form.password.value,
+    };
+
+    if(store_data===null)
+    {
+        alert("Please create an account!!");
+    }
+    else if(store_data.email===data.email && store_data.password===data.password){
+        alert("Sign In Sucessfully");
+        window.location.href="./index.html";
+       
+    }
+
+    else{
+        alert("User Does not exixt")
+    }
+});
