b2c14130c92f65c4f33e74f669e1f66cdabf623e
GauriSandhan <89839207+GauriSandhan@users.noreply.github.com>
2022-11-13 17:40:35 +0530

    done

--- Script/prod.js
+++ Script/prod.js
@@ -7,10 +7,23 @@ img.setAttribute("class","banner")
 img.src=que.image;
 let name=document.createElement("p")
 name.innerText=que.name;
+name.setAttribute("class","nam")
+
 let description=document.createElement("p")
 description.innerText=que.description;
-let date=document.createElement("p")
-date.innerText=que.date;
+description.setAttribute("class","desc")
+
 let price=document.createElement("p")
-price.innerText=que.price;
-box.append(img,description,date,price,name)
+price.innerText=`₹  ${que.price}`;
+
+
+
+price.setAttribute("class","price")
+let date=document.createElement("p")
+date.innerText=`Date - ${que.date}`;
+
+
+date.setAttribute("class","date")
+
+
+box.append(img,price,date,description,name)
